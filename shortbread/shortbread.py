5290b6533c5d9b3fd50f43c86a26e0f65cffff87
Diane Chen <dianedc@purpleweb.com>
2018-03-10 09:20:49 -0800

    Add the shortbread.py code.

--- shortbread/shortbread.py
+++ shortbread/shortbread.py
@@ -1,3 +1,11 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+
+
+def shortbread(unit):
+    """Prints ingredients for making `unit` dozen shortbread cookies."""
+    print(f'{unit} cups flour')
+    print(f'{.25 * unit} cups sugar')
+    print(f'{.5 * unit} cups butter')
+    print(f'Bake at 350 degrees for 10-12 minutes')
