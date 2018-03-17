8d4b989dff278829a0d4586c1c9b8dccf4460283
Diane Chen <dianedc@purpleweb.com>
2018-03-17 15:01:56 -0700

    Improve tests for long instructions and chocolate chips

--- tests/test_shortbread.py
+++ tests/test_shortbread.py
@@ -27,27 +27,59 @@ def test_content(response):
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
 
 
-def test_command_line_interface():
-    """Test the CLI."""
+def test_command_line_interface_simple():
+    """Test the CLI in the simplest version."""
     runner = CliRunner()
-    # Original that cookiecutter created
-    # result = runner.invoke(cli.main)
-    # assert result.exit_code == 0
-    # assert 'shortbread.cli.main' in result.output
-    # help_result = runner.invoke(cli.main, ['--help'])
-    # assert help_result.exit_code == 0
-    # assert '--help  Show this message and exit.' in help_result.output
+
     result = runner.invoke(cli.main, ['4'])
     assert result.exit_code == 0
+    assert 'Shortbread Cookies - makes approx. 4 doz.' in result.output
     assert '4 cups flour' in result.output
     assert '1.0 cups sugar' in result.output
     assert '2.0 cups salted butter' in result.output
+    assert '1.0 teaspoons vanilla (optional)' in result.output
     assert 'Bake cookies at 350 degrees for 10-12 minutes' in result.output
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
     # assert 'Usage: shortbread [OPTIONS] COUNT' in help_result.output
     # Can't use that ^^^ because it says Usage: main here.
     assert '--help      Show this message and exit.' in help_result.output
+
+
+def test_command_line_interface_long():
+    """Test the CLI in the simplest version."""
+    runner = CliRunner()
+
+    result = runner.invoke(cli.main, ['4', '-l'])
+    assert result.exit_code == 0
+    assert 'Shortbread Cookies - makes approx. 4 doz.' in result.output
+    assert '- Preheat oven to 350 degrees F.' in result.output
+    assert '- Bake 10 to 12 minutes at 350 degrees' in result.output
+
+
+def test_command_line_interface_chocolate():
+    """Test the CLI in the simplest version."""
+    runner = CliRunner()
+
+    result = runner.invoke(cli.main, ['4', '-c'])
+    assert ('Chocolate Chip Shortbread Cookies - makes approx. 4 doz.'
+            in result.output)
+    assert result.exit_code == 0
+
+
+def test_command_line_interface_chocolate_long():
+    """Test the CLI in the simplest version."""
+    runner = CliRunner()
+
+    result = runner.invoke(cli.main, ['4', '-l', '-c'])
+    assert ('Chocolate Chip Shortbread Cookies - makes approx. 4 doz.'
+            in result.output)
+    assert result.exit_code == 0
+    assert '- Sift together baking powder and flour.' in result.output
+    assert '- Stir in the chocolate chips.' in result.output
+    assert '- Flatten cookie balls slightly.' in result.output
+    assert ('- Bake 15 to 20 minutes at 350 degrees until golden brown.'
+            in result.output)
 
 
 def test_shortbread(capsys):
@@ -57,6 +89,7 @@ def test_shortbread(capsys):
     # Capture the result of the arepas.ingredients() function call.
     captured = capsys.readouterr()
     # If we check captured, we can see that the ingredients have been printed.
+    assert 'Shortbread Cookies - makes approx. 4 doz.' in captured.out
     assert "4 cups flour" in captured.out
     assert "1.0 cups sugar" in captured.out
     assert "2.0 cups salted butter" in captured.out
